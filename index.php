4a96b4c2a2fa808763650edc50d4d12b1c70b754
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-09-12 11:32:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>David CRAVO</title>
+        <link rel="stylesheet" href="src/css/styleHome.css">
+        <link rel="stylesheet" href="/src/css/styleHeader.css">
+        <link rel="stylesheet" href="/src/css/styleFooter.css">
+        <link rel="icon" href="src/img/logoCravoDavid.png">
+    </head>
+
+    <body>
+
+        <header>
+            <div class="gradient">
+                <a href="../index.html">
+                    <img src="/src/img/logoCravoDavid.png" alt="Logo de David CRAVO">
+                </a>
+                <nav >
+                    <ul>
+                        <li><a class="currentlink" href="index.html">Acceuil</a></li>
+                        <li><a href="/pages/profile.html">Profil</a></li>
+                        <li><a href="/pages/experience.html">Expérience</a></li>
+                        <li><a href="/pages/education.html">Formations</a></li>
+                        <li><a href="/pages/skills.html">Compétences</a></li>
+                        <li><a href="/pages/interests.html">Loisirs</a></li>
+                        <li><a href="/pages/achievements.html">Réalisations</a></li>
+                        <li><a href="/pages/contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <main>
+            <h1 class="gradientTitle">Bonjour !</h1>
+            <article>
+                <img src="src/img/home/photoProfilCroppedResized500.jpg" alt="Photo de profil">
+                <div>
+                    <a class="cv" href="/src/pdf/CRAVO_David_CV_2024 Version_2.7.pdf" target="_blank">Mon Curriculum Vitae</a><br>
+                    <a class="cv" href="/src/pdf/Plan_de_formation_MS2D.pdf" target="_blank">Plan de formation Manager de Solutions Digitales et Data</a><br>
+                    <a class="cv" href="/src/pdf/Calendrier_MS2D.pdf" target="_blank">Calendrier de formation Manager de Solutions Digitales et Data</a><br>
+                </div>
+                <!-- <button class="boutonAcceuil" type="button">
+                    <a href="pages/profile.html">Entrez !</a> 
+                </button> -->
+            </article>
+        </main>
+    </body>
+    
+</html>
---
+++ src/js/scriptContact.js
@@ -0,0 +1,11 @@
+let form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let firstname = document.getElementById("firstname")
+    let lastname = document.getElementById("lastname")
+    let role = document.getElementById("role")
+    let company = document.getElementById("company")
+    let email = document.getElementById("email")
+    let message = document.getElementById("message")
+})
