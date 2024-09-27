76c5a299641609c3295fe1a62003122f72abb1c5
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-09-27 10:54:58 +0200

    Redirection vers de index.php vers home.php

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr-fr">
+<?php
+header('Location: /pages/home.php');
+?>
 
-    <?php 
-        $title = 'David CRAVO';
-        require 'components/head.php';
-    ?>
-
-    <body>
-        <?php require 'components/header.php'?>
-        <main class="homeMain">
-            <h1 class="gradientTitle">Bonjour !</h1>
-            <article class="homeArticle">
-                <img class="homeImage" src="src/img/home/photoProfilCroppedResized500.jpg" alt="Photo de profil">
-                <div class="homeDocuments">
-                    <a class="document" href="/src/pdf/CRAVO_David_CV_2024 Version_2.7.pdf" target="_blank">Mon Curriculum Vitae</a><br>
-                    <a class="document" href="/src/pdf/Plan_de_formation_MS2D.pdf" target="_blank">Plan de formation Manager de Solutions Digitales et Data</a><br>
-                    <a class="document" href="/src/pdf/Calendrier_MS2D.pdf" target="_blank">Calendrier de formation Manager de Solutions Digitales et Data</a><br>
-                </div>
-                <!-- <button class="boutonAcceuil" type="button">
-                    <a href="pages/profile.html">Entrez !</a> 
-                </button> -->
-            </article>
-        </main>
-    </body>
-    
-</html>
