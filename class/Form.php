7038c1865067d0e61df8fc557f913dcbb3b522bc
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-09-27 16:41:25 +0200

    Update formulaire de contact

---
+++ class/Form.php
@@ -0,0 +1,55 @@
+<?php
+class Form{
+
+    private $data = [];
+
+    public function __construct($data = []){
+        $this->data = $data;
+        // var_dump($this->$data);
+    }
+
+    private function getValue($name){
+        $value = "";
+        if(isset($this->data[$name])){
+            $value = $this->data[$name];
+        }
+        return $value;
+    }
+
+    private function input($type, $name, $label){
+        $value = $this->getValue($name);
+        if ($type == 'textarea'){
+            $input = "<textarea name=\"$name\" id=\"$name\">$value</textarea>";
+        }else{
+            $input = "<input type=\"$type\" id=\"$name\" name=\"$name\" value=\"$value\">";
+        }
+        return "<label for=\"$name\">$label *</label>
+        $input";
+    }
+
+    public function inputText($name, $label){
+        return $this->input('text', $name, $label);
+    }
+
+    public function inputEmail($name, $label){
+        return $this->input('email', $name, $label);
+    }
+
+    public function select($name, $label, $options){
+        $options_html = "";
+        $value = $this->getValue($name);
+        foreach($options as $k => $v){
+            $selected = "";
+            if($value == $k){
+                $selected = ' selected';
+            }
+            $options_html .= "<option value=\"$k\"$selected>$v</option>";
+        }
+        return "<label for\"$name\">$label</label>
+            <select name=\"$name\" id=\"$name\">$options_html</select>";
+    }
+
+    public function textarea($name, $label){
+        return $this->input('textarea', $name, $label);
+    }
+}
--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,5 @@
 <?php
-    include 'components/includes.php';
+    include '../components/includes.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr-fr">
@@ -23,31 +23,32 @@
                     <div>
                         Votre message a bien été envoyé.
                     </div>
-                    <?php unset($_SESSION['success']); ?>
                 <?php endif; ?>
 
                 <form action="../components/postContact.php" method="POST">
-                    <label for="firstname">Prénom *</label>
-                    <input type="text" id="firstname" name="firstname" value="<?= isset($_SESSION['inputs']['firstname']) ? $_SESSION['inputs']['firstname'] : '';?>">
-                    <label for="lastname">Nom *</label>
-                    <input type="text" id="lastname" name="lastname" value="<?= isset($_SESSION['inputs']['lastname']) ? $_SESSION['inputs']['lastname'] : '';?>">
-                    <label for="role">Fonction *</label>
-                    <input type="text" name="role" id="role" value="<?= isset($_SESSION['inputs']['role']) ? $_SESSION['inputs']['role'] : '';?>">
-                    <label for="company">Entreprise *</label>
-                    <input type="text" name="company" id="company" value="<?= isset($_SESSION['inputs']['company']) ? $_SESSION['inputs']['company'] : '';?>">
-                    <label for="email">Email *</label>
-                    <input type="email" name="email" id="email" value="<?= isset($_SESSION['inputs']['email']) ? $_SESSION['inputs']['email'] : '';?>">
-                    <label for="message">Message *</label>
-                    <textarea name="message" id="message"><?= isset($_SESSION['inputs']['message']) ? $_SESSION['inputs']['message'] : '';?></textarea>
+                    <?php
+                        //var_dump($_SESSION);
+                        $form = new Form(isset($_SESSION['inputs']) ? $_SESSION['inputs'] : []);
+                        $inputValues = [
+                            'firstname'=>'Prénom',
+                            'lastname'=>'Nom',
+                            'role'=>'Fonction',
+                            'company'=>'Entreprise'
+                        ];
+                        foreach($inputValues as $k => $v){
+                            echo $form->inputText($k, $v);
+                        }
+                    ?>
+                    <?= $form->inputEmail('email', 'Email'); ?>
+                    <?= $form->select('object', 'Objet', ['Stage', 'Alternance', 'Emploi', 'Autre']); ?>
+                    <?= $form->textarea('message', 'Message'); ?>
                     <button type="submit">Envoyer</button>
                 </form>
-                <h2>Debug :</h2>
-                <?= var_dump($_SESSION); ?>
             </div>
-            <aside class="contactAside">
-                <a href="https://www.linkedin.com/in/davidcravo" target="_blank"><img src="/src/img/contact/linkedIn.png" alt="linkedIn"></a>
-                <a href="https://www.facebook.com/david.cravo.37" target="_blank"><img src="/src/img/contact/facebook.png" alt="facebook"></a>
-            </aside>
+            <!-- <aside class="contactAside"> -->
+                <!-- <a href="https://www.linkedin.com/in/davidcravo" target="_blank"><img src="/src/img/contact/linkedIn.png" alt="linkedIn"></a> -->
+                <!-- <a href="https://www.facebook.com/david.cravo.37" target="_blank"><img src="/src/img/contact/facebook.png" alt="facebook"></a> -->
+            <!-- </aside> -->
             
         </main>
         
@@ -56,6 +57,6 @@
 
 <?php 
 unset($_SESSION['errors']); 
-unset($_SESSION['inputs']);
+// unset($_SESSION['inputs']);
 unset($_SESSION['success'])
 ?>
