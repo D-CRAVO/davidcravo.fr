8fb7ea2c213b677c028ec39618d9e24b34326f49
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-12 15:30:26 +0200

    Footer header : ajout des reseaux sociaux

--- components/header.php
+++ components/header.php
@@ -1,10 +1,17 @@
-<?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'navigation.php' ?>
+<?php 
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'constants.php';
+    require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'navigation.php';
+?>
 
 <header>
     <div class="gradient">
         <a href="../templates/home.php">
             <img src="../assets/images/david_cravo_logo.jpeg" alt="Logo de David CRAVO">
         </a>
+        <div class="links">
+            <a href="<?= LINKS['github'] ?>" target="_blank"><img src="../assets/images/header/github.png" alt="logo Github"></a>
+            <a href="<?= LINKS['linkedin'] ?>" target="_blank"><img src="/assets/images/header/linkedin.png" alt="logo Linkedin"></a>
+        </div>
         <nav >
             <ul>
                 <?= nav_item('/templates/home.php', 'Acceuil') ?>
